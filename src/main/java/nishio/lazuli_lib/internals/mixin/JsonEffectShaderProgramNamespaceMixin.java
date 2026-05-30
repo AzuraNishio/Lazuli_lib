cccd07dab7a0a6728efbef6d0b91a96540856c3f
megastevian <prankermats@gmail.com>
2026-05-30 21:37:16 +0200

    Tried adding compat with architectury-api by making the mod use an @WrapOperation instead of an @Redirect

--- src/main/java/nishio/lazuli_lib/internals/mixin/JsonEffectShaderProgramNamespaceMixin.java
+++ src/main/java/nishio/lazuli_lib/internals/mixin/JsonEffectShaderProgramNamespaceMixin.java
@@ -1,43 +1,77 @@
 package nishio.lazuli_lib.internals.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.metadata.ModDependency;
 import net.minecraft.client.gl.JsonEffectShaderProgram;
 import net.minecraft.client.gl.ShaderStage;
 import net.minecraft.resource.ResourceFactory;
 import net.minecraft.util.Identifier;
+import nishio.lazuli_lib.internals.Lazuli_Lib;
+import nishio.lazuli_lib.internals.Lazuli_Lib_Client;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(JsonEffectShaderProgram.class)
+import java.lang.reflect.Type;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+@Mixin(value = JsonEffectShaderProgram.class, priority = 1000)
 public class JsonEffectShaderProgramNamespaceMixin {
+    @Unique
+    private static final Set<String> OPT_IN_MODS = FabricLoader.getInstance()
+            .getAllMods()
+            .stream()
+            .filter(
+                    it -> it.getMetadata()
+                            .getDependencies()
+                            .stream()
+                            .anyMatch(dep -> dep.getKind().equals(ModDependency.Kind.DEPENDS) && dep.getModId().equals("<mod_id_here>"))
+            )
+            .map(it -> it.getMetadata().getId())
+            .collect(Collectors.toSet());
 
-    @Redirect(
+    @WrapOperation(
             method = "loadEffect",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/util/Identifier;ofVanilla(Ljava/lang/String;)Lnet/minecraft/util/Identifier;"
             )
     )
-    private static Identifier redirectIdentifierCreation(String path, ResourceFactory resourceFactory, ShaderStage.Type type, String name) {
+    private static Identifier redirectIdentifierCreation(String path, Operation<Identifier> original, ResourceFactory resourceFactory, ShaderStage.Type type, String name) {
+
+    //okay I wanted to say this for anyone who reads this code but genuinely the fabric discord was so helpful to me on this -matscalle
+
+        Lazuli_Lib_Client.LOGGER.info(type.getFileExtension());
+        Lazuli_Lib_Client.LOGGER.info("BELOW IS AN IMPORTANT LIST TO SEE WHATS GOING ON");
+        Lazuli_Lib_Client.LOGGER.info(name);
+        Lazuli_Lib_Client.LOGGER.info(type.getName());
+        Lazuli_Lib_Client.LOGGER.info(type.toString());
+        Lazuli_Lib_Client.LOGGER.info(path.replace("lazuli_lib:", ""));
 
         if (name.contains(":")) {
             Identifier parsedId = Identifier.of(name);
+            if (!OPT_IN_MODS.contains(parsedId.getNamespace())) {
 
-            if (name.contains("post/")) {
+                if (name.contains("post/")) {
+                    return Identifier.of(
+                            parsedId.getNamespace(),
+                            "shaders/" + parsedId.getPath() + type.getFileExtension()
+                    );
+                }
+
                 return Identifier.of(
                         parsedId.getNamespace(),
-                        "shaders/" + parsedId.getPath() + type.getFileExtension()
+                        "shaders/program/" + parsedId.getPath() + type.getFileExtension()
                 );
             }
-
-            return Identifier.of(
-                    parsedId.getNamespace(),
-                    "shaders/program/" + parsedId.getPath() + type.getFileExtension()
-            );
         }
 
         // Fall back to vanilla behavior
-        return Identifier.ofVanilla(path);
+        return original.call(path);
     }
 }
